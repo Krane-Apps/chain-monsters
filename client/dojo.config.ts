01f322a1dbda7ab02d1bbd7026c7e66b2ba2b355
bal7hazar <bal7hazar@proton.me>
2024-06-22 12:21:25 +0200

    🚀 Deploy a new version on slot

--- client/dojo.config.ts
+++ client/dojo.config.ts
@@ -1,4 +1,5 @@
 import local from "../contracts/manifests/dev/manifest.json";
+import slot from "../contracts/manifests/slot/manifest.json";
 
 const {
   VITE_PUBLIC_NODE_URL,
@@ -7,7 +8,7 @@ const {
   VITE_PUBLIC_MASTER_PRIVATE_KEY,
   VITE_PUBLIC_ACCOUNT_CLASS_HASH,
   VITE_PUBLIC_FEE_TOKEN_ADDRESS,
-  VITE_PUBLIC_PRODUCTION,
+  VITE_PUBLIC_SLOT,
 } = import.meta.env;
 
 export type Config = ReturnType<typeof dojoConfig>;
@@ -28,6 +29,6 @@ export function dojoConfig() {
     feeTokenAddress:
       VITE_PUBLIC_FEE_TOKEN_ADDRESS ||
       "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-    manifest: VITE_PUBLIC_PRODUCTION ? local : local,
+    manifest: VITE_PUBLIC_SLOT ? slot : local,
   };
 }
